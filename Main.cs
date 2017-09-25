8ffc0825960ea52795170409c6a62993b92d8f11
Joseph Flanagan <jflanag5@email.cpcc.edu>
2017-09-24 21:52:59 -0400

    Added two comments above 2 randomly selected Quotes in the master Repo

--- Main.cs
+++ Main.cs
@@ -35,7 +35,8 @@ public class SGD285_1
         UserQuote[] userQuotes = new UserQuote[] 
 	    {
            new UserQuote("Bahram Dahi", "I have never let my schooling interfere with my education. - [attributed to] Mark Twain"),
-	       new UserQuote("Ryan Douglass", "If there is no struggle there is no progress. - Frederick Douglass"),
+           /*Failing is learning!*/
+           new UserQuote("Ryan Douglass", "If there is no struggle there is no progress. - Frederick Douglass"),
 	       new UserQuote("Baylen Smith", "Whatever happens, happens. - Spike Spiegel"),
 	       new UserQuote("Jennifer Ethridge", "I have found a desire within myself that no experience in this world can satisfy; the most probable explanation is that I was made for another world."),
            new UserQuote("Haley Sessions", "No road is long with good company. - Turkish Proverb"),
@@ -44,7 +45,8 @@ public class SGD285_1
            new UserQuote("Kendall Wix", "At the end of this day, one shall stand, one shall fall!. O - Optimus Prime"),	
            new UserQuote("Joseph Mitchell", "Education is a weapon, whose effect depends on who holds it in his hands and whom it is aimed. - Joseph Stalin"),					
            new UserQuote("Preston Rockholt", "I am the wisest man alive, for I know one thing, and that is that I know nothing. - [attributed to] Socrates"),
-	       new UserQuote("Jonathon Huskey", "The path of least resistance is the path of the loser. -H. G. Wells"),
+            /*This doesnt include copy and pasting does it cause pasting makes this so much easier*/
+           new UserQuote("Jonathon Huskey", "The path of least resistance is the path of the loser. -H. G. Wells"),
            new UserQuote("Jennifer Baldwin", "I am no bird; and no net ensnares me; I am a free human being with an independent will. - Charlotte Bronte")
            // add the next user here using the formula above, don't forget the comma to separate the items in the initialization list. 
            // IMPORTANT: If there is a conflict, you MUST fix it, compile to make sure 
